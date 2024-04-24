b32d290dcfe41ac46db2be318b60a86e0c2866e3
Henry Lee <henryleeworld@gmail.com>
2024-04-25 00:18:55 +0800

    feat:更新最新資料

--- resources/views/daily-report.blade.php
+++ resources/views/daily-report.blade.php
@@ -7,34 +7,34 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
-        <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css" />
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css" />
+        <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css" />
     </head>
     <body>
         <div class="container-fluid col-sm-12 mt-3">
             <table id="daily-breakdown-vaccination-by-district-report" class="table table-striped cell-border compact" style="width: 100%;">
                 <thead>
                     <tr>
-                        <th>發佈統計日期</th>
-                        <th>縣市</th>
-                        <th>年齡層</th>
-                        <th>第 1 劑接種率</th>
-                        <th>第 2 劑接種率</th>
-                        <th>基礎加強劑接種率</th>
-                        <th>第 1 次追加劑接種率</th>
-                        <th>第 2 次追加劑接種率</th>
-                        <th>第 3 次追加劑接種率</th>
-                        <th>第 4 次追加劑接種率</th>
-                        <th>第 5 次追加劑接種率</th>
+                        <th>{{ __('Release statistics date') }}</th>
+                        <th>{{ __('City') }}</th>
+                        <th>{{ __('Age group') }}</th>
+                        <th>{{ __('1st dose vaccination rate') }}</th>
+                        <th>{{ __('2nd dose vaccination rate') }}</th>
+                        <th>{{ __('Basic booster vaccination rate') }}</th>
+                        <th>{{ __('1st booster vaccination rate') }}</th>
+                        <th>{{ __('2nd booster vaccination rate') }}</th>
+                        <th>{{ __('3rd booster vaccination rate') }}</th>
+                        <th>{{ __('4th booster vaccination rate') }}</th>
+                        <th>{{ __('5th booster vaccination rate') }}</th>
                     </tr>
                 </thead>
             </table>
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js" integrity="sha512-VK2zcvntEufaimc+efOYi622VN5ZacdnufnmX7zIhCPmjhKnOi9ZDMtg1/ug5l183f19gG1/cBstPO4D8N/Img==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
         <script>
             $(function () {
                 $.extend(true, $.fn.dataTable.Buttons.defaults.dom.button, {
@@ -47,7 +47,7 @@
 
                 $("#daily-breakdown-vaccination-by-district-report").DataTable({
                     language: {
-                        url: "https://cdn.datatables.net/plug-ins/1.13.4/i18n/zh-HANT.json",
+                        url: "https://cdn.datatables.net/plug-ins/1.13.5/i18n/zh-HANT.json",
                     },
                     buttons: dtButtons,
                     dom: 'lBfrtip<"actions">',
